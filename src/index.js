5cf04c20208333300d41dd26daeb31bf604eb3a0
buiquangson123 <buiquangson19091998@gmail.com>
2022-02-20 21:22:02 +0700

    up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 const host = "0.0.0.0";
 app.listen(PORT, host, () => {
   console.log(`App listening at http://localhost:${PORT}`);
